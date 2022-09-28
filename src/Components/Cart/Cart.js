31b4fcbba0afa57b813a5b63c33e1104346cf0a3
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-09-28 18:31:15 +0800

    first commit

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({ cart }) => {
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total += product.price;
+        shipping += product.shipping;
+    }
+    const tax = parseFloat((total * .1).toFixed(2));
+    const grandTotal = total + shipping + tax;
+
+    return (
+        <div className='cart'>
+            <h4>Order Summary</h4>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+        </div>
+    );
+};
+
+export default Cart;
